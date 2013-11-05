46252dc1374d2ba693d8d824a235e8317595bc19
Kevin Conley <kevindconley@gmail.com>
2013-11-04 18:58:31 -0800

    Updates to get crontab working on desktop

--- stats.sh
+++ stats.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-source venv/bin/activate
-python stats.py
+
+export DOTABOT_USERNAME=dotabot@stanford.edu
+export DOTABOT_HOSTNAME=mail.stanford.edu
+export DOTABOT_API_KEY=EB2511951A4ED23C75D5FCDFE39692ED
+
+source /home/kcon/dota2project/venv/bin/activate
+python /home/kcon/dota2project/stats.py
