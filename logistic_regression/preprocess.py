ee46533d7a4bcb3b3709b7e647d604e62ae0565c
Kevin Conley <kevindconley@gmail.com>
2013-11-14 01:34:02 -0800

    Adding basic logistic regression experiment with k-fold cross validation

---
+++ logistic_regression/preprocess.py
@@ -0,0 +1,60 @@
+import csv, pickle
+import numpy as np
+
+np.set_printoptions(threshold=np.nan)
+
+# We're going to create a training matrix, X, where each
+# row is a different match and each column is a feature
+
+# The features are bit vectors indicating whether heroes
+# were picked (1) or not picked (0). The first 103 features
+# correspond to radiant, and the last 103 features are
+# for dire.
+
+NUM_HEROES = 103
+NUM_FEATURES = NUM_HEROES * 2
+
+# Our training label vector, Y, is a bit vector indicating
+# whether radiant won (1) or lost (0)
+
+with open('test.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    matches = list(reader)
+
+    NUM_MATCHES = sum(1 for row in matches)
+
+    # REMOVE ME
+    #NUM_MATCHES = 25
+
+    # Initialize training matrix
+    X = np.zeros((NUM_MATCHES, NUM_FEATURES), dtype=np.int8)
+
+    # Initialize training label vector
+    Y = np.zeros(NUM_MATCHES, dtype=np.int8)
+
+    for i, record in enumerate(matches):
+        # REMOVE ME
+        #if i >= 25:
+        #    break
+
+        Y[i] = record['radiant_win'] == 'true'
+
+        for player_index in range(10):
+            player_slot_field = 'players.%d.player_slot' % player_index
+            player_slot = int(record[player_slot_field])
+
+            hero_id_field = 'players.%d.hero_id' % player_index
+            hero_id = int(record[hero_id_field])
+            hero_id_index = hero_id - 1
+
+            # If the left-most bit of player_slot is set,
+            # this player is on dire, so push the index accordingly
+            if player_slot >= 128:
+                hero_id_index += NUM_HEROES
+
+            X[i, hero_id_index] = 1
+
+    with open('processed.pkl', 'wb') as pickle_file:
+        # Pickle X first, then Y
+        pickle.dump(X, pickle_file)
+        pickle.dump(Y, pickle_file)
