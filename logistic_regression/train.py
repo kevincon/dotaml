f0b0d69f34d8c2b0741fae84a00616838c5830a1
Kevin Conley <kevindconley@gmail.com>
2013-12-08 23:43:34 -0800

    Fixed bug in old logistic regression.

--- logistic_regression/train.py
+++ logistic_regression/train.py
@@ -1,23 +1,11 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn import cross_validation
-from functools import partial
 import numpy as np
-#import pickle
-
-def argmax(xs, f, **kwargs):
-    '''Returns (argmax, max_value).'''
-    max_value = None
-    scores = map(partial(f, **kwargs), xs)
-    max_value = max(scores)
-    return (xs[scores.index(max_value)], max_value)
-
 
 # Import the preprocessed X matrix and Y vector
 preprocessed = np.load('processed.npz')
 X = preprocessed['X']
 Y = preprocessed['Y']
-#X = pickle.load(preprocessed_file)
-#Y = pickle.load(preprocessed_file)
 
 NUM_MATCHES = len(X)
 print 'Training using data from %d matches...' % NUM_MATCHES
@@ -25,8 +13,14 @@ print 'Training using data from %d matches...' % NUM_MATCHES
 k_fold = cross_validation.KFold(n=NUM_MATCHES, n_folds=10, indices=True)
 
 # Find the Logistic Regression model with maximum accuracy on its test set
-models = [LogisticRegression().fit(X[train], Y[train]) for train, test in k_fold]
-best_model, score = argmax(models, LogisticRegression.score, X=X[test], y=Y[test])
+score_model_pairs = []
+for train, test in k_fold:
+    model = LogisticRegression().fit(X[train], Y[train])
+    score = model.score(X[test], Y[test])
+    score_model_pairs.append((score, model))
+
+ranked = sorted(score_model_pairs)
+
+score, best_model = ranked[0]
 
 print 'Best model achieved %f accuracy on its test set.' % score
-print 'Best model achieved %f accuracy on entire dataset.' % best_model.score(X, Y)
