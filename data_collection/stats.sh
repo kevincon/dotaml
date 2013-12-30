553317a2e00cfdcd1886d5f5cb2742432ed71d65
Kevin Conley <kevindconley@gmail.com>
2013-12-30 01:15:36 -0800

    Removed old files

--- data_collection/stats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DOTABOT_USERNAME=dotabot@stanford.edu
-export DOTABOT_HOSTNAME=mail.stanford.edu
-export DOTABOT_API_KEY=EB2511951A4ED23C75D5FCDFE39692ED
-
-source /home/kcon/dota2project/venv/bin/activate
-python /home/kcon/dota2project/stats.py
